f2f42d6f1ea0f572534538b5a6f10e4ea01d5d3c
Brandon Culp <bculp513@gmail.com>
2016-09-20 11:21:13 -0400

    completed section 6

---
+++ tweetBank.js
@@ -0,0 +1,60 @@
+var _ = require('lodash');
+
+var data = [];
+
+function add (name, content) {
+  data.push({ name: name, content: content });
+}
+
+function list () {
+  return _.cloneDeep(data);
+}
+
+function find (properties) {
+  return _.cloneDeep(_.filter(data, properties));
+}
+
+module.exports = { add: add, list: list, find: find };
+
+
+
+var randArrayEl = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var getFakeName = function() {
+  var fakeFirsts = ['Nimit', 'Dave', 'Shanna', 'Charlotte', 'Scott', 'Ayana', 'Omri', 'Gabriel', 'Joe'];
+  var fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+
+var getFakeTweet = function() {
+  var awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing'];
+  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+};
+
+for (var i = 0; i < 10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet() );
+}
+
+data = [ { name: 'Gabriel OLogn',
+    content: 'Fullstack Academy is wonderful! The instructors are just so wonderful. #fullstacklove #codedreams' },
+  { name: 'Joe Dunderproto',
+    content: 'Fullstack Academy is cool! The instructors are just so funny. #fullstacklove #codedreams' },
+  { name: 'Ayana Binder',
+    content: 'Fullstack Academy is wonderful! The instructors are just so sweet. #fullstacklove #codedreams' },
+  { name: 'Scott Claujure',
+    content: 'Fullstack Academy is breathtaking! The instructors are just so amazing. #fullstacklove #codedreams' },
+  { name: 'Scott OLogn',
+    content: 'Fullstack Academy is wonderful! The instructors are just so wonderful. #fullstacklove #codedreams' },
+  { name: 'Dave Stackson',
+    content: 'Fullstack Academy is mindblowing! The instructors are just so amazing. #fullstacklove #codedreams' },
+  { name: 'Nimit McQueue',
+    content: 'Fullstack Academy is awesome! The instructors are just so cool. #fullstacklove #codedreams' },
+  { name: 'Scott Ternary',
+    content: 'Fullstack Academy is amazing! The instructors are just so mindblowing. #fullstacklove #codedreams' },
+  { name: 'Joe OLogn',
+    content: 'Fullstack Academy is mindblowing! The instructors are just so amazing. #fullstacklove #codedreams' },
+  { name: 'Dave OLogn',
+    content: 'Fullstack Academy is awesome! The instructors are just so breathtaking. #fullstacklove #codedreams' } ]
+console.log(find({'name' : "Dave Stackson"}));
